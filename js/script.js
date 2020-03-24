9286f51846901c698174d5b04760360c4217075f
DanielPetelin <daniilpetelin77@gmail.com>
2020-03-24 10:27:11 +0300

    add new

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
 let modal = document.querySelector('.modal');
 let btnHide = document.querySelector('#hide');
 let btnShow = document.querySelector('#show');
+
+function show() {
+    modal.classList.remove('hide');
+    modal.classList.add('show');
+};
+
+function hide() {
+    modal.classList.remove('show');
+    modal.classList.add('hide');
+};
+
+btnShow.onclick = show;
+btnHide.onclick = hide;
